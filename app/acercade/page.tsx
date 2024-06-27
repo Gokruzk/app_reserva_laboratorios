096c33a1e38bd1658a31e3a6df6b2f6a7b620574
Gokruzk <nigell@gmail.com>
2024-06-27 10:55:54 -0500

    cambios

---
+++ app/acercade/page.tsx
@@ -0,0 +1,6 @@
+
+export default function AcercaDe() {
+  return (
+    <div>Acerca de</div>
+  )
+}
--- app/historial/page.tsx
+++ app/historial/page.tsx
@@ -1,14 +1,9 @@
-import Image from "next/image";
-import Link from "next/link";
-import LinkButton from "@/components/LInkButton";
-
-
 export default function Historial() {
   return (
     <main className="bg-gray-50 dark:bg-gray-900">
       
       <div>
-       
+       Historial
       </div>
     </main>
   );
